36c5ddc98a59c4dd71501fac24cfba16dc7399a4
unknown <you@example.com>
2021-10-02 01:08:29 +0500

    Manipulating Websites Using JS Window Object

---
+++ app4.js
@@ -0,0 +1,21 @@
+console.log('shairyar is the best');
+
+let arr2 = [1,3,5,7,9]
+
+// for(i=0; i< arr2.length; i++){
+//     console.log(arr2);
+// }
+
+arr2.forEach(function(element){
+    console.log(element)
+})
+
+// window.alert('welcome here');
+
+// let a = window;
+// a = prompt('enter your age');
+// console.log(a)
+
+// a = confirm('are you married?');
+a = location;
+console.log(a);
