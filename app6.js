8ce0d70d918b7a7e94afaa96d00a6b47de77cd51
unknown <you@example.com>
2021-10-04 04:40:28 +0500

    DOM selecting HTML elements

---
+++ app6.js
@@ -0,0 +1,34 @@
+// Loops practice
+// console.log('hi');
+
+// let arr1 = ['shairyar ', ' ali',4,6,8,10,65468, true]
+// let i=0
+// while(i<arr1.length){
+//     console.log(arr1[i])
+//     i++
+// }
+
+
+// for(i=0; i<arr1.length; i++){
+//     console.log(arr1[i]);
+//     // document.write(arr1[i]);
+// }
+
+// for(i=0; i<arr1.length; i++){
+//     console.log(arr1[i])
+// }
+// const veges = ['pumpkin', 'beans', 'patato']
+// veges.forEach(function(element){
+//     console.log(element);
+// })
+
+
+// veges.forEach(function(vegetable){
+//     console.log(vegetable);
+// })
+
+let selection = document.getElementById('first')
+// selection = selection.className
+
+selection.style.color = 'red';
+console.log(selection);
